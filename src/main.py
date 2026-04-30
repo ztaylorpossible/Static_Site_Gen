eaf6676ed5ef1d5d87731c1fd1be9e98fd04f12d
ztaylorpossible <zachtaylorstudios@gmail.com>
2026-04-30 12:33:21 -0700

    feat: Start implementing a text node object

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
