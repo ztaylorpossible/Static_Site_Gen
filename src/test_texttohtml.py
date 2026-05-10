f0dc68cc9a50deb9c962753b27898607fe995e8a
ztaylorpossible <zachtaylorstudios@gmail.com>
2026-05-09 20:08:03 -0700

    feat: Parse markdown blocks into html nodes

--- src/test_texttohtml.py
+++ src/test_texttohtml.py
@@ -1,5 +1,5 @@
 import unittest
-from texttohtml import text_node_to_html_node
+from texttohtml import text_node_to_html_node, markdown_to_html_node
 from textnode import TextNode, TextType
 
 class TestTextToHTML(unittest.TestCase):
@@ -55,3 +55,86 @@ class TestTextToHTML(unittest.TestCase):
         if html_node.props:
             self.assertEqual(html_node.props["src"], "meme.jpg")
             self.assertEqual(html_node.props["alt"], "This is an image node")
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>"
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>"
+        )
+
+    def test_headers(self):
+        md = "# This is an h1 header"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h1>This is an h1 header</h1></div>")
+        md = "## This is an h2 header"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h2>This is an h2 header</h2></div>")
+        md = "### This is an h3 header"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h3>This is an h3 header</h3></div>")
+        md = "#### This is an h4 header"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h4>This is an h4 header</h4></div>")
+        md = "##### This is an h5 header"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h5>This is an h5 header</h5></div>")
+        md = "###### This is an h6 header"
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h6>This is an h6 header</h6></div>")
+
+    def test_blockquote(self):
+        md = """
+> This is a quote
+> and a **second** line of quote.
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><blockquote>This is a quote and a <b>second</b> line of quote.</blockquote></div>")
+
+    def test_unordered_list(self):
+        md = """
+- This is one item
+- This is _another_ item
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><ul><li>This is one item</li><li>This is <i>another</i> item</li></ul></div>")
+
+    def test_ordered_list(self):
+        md = """
+1. This is one item
+2. This is `another` item
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><ol><li>This is one item</li><li>This is <code>another</code> item</li></ol></div>")
--- src/texttohtml.py
+++ src/texttohtml.py
@@ -1,5 +1,8 @@
 from textnode import TextNode, TextType
 from leafnode import LeafNode
+from parentnode import ParentNode
+from converttext import markdown_to_blocks, text_to_textnodes
+from blocktype import BlockType, block_to_block_type
 
 def text_node_to_html_node(text_node):
     if type(text_node) != TextNode:
@@ -18,3 +21,67 @@ def text_node_to_html_node(text_node):
         return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
     raise ValueError()
 
+def markdown_to_html_node(md):
+    blocks = markdown_to_blocks(md)
+    nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.CODE:
+            text_node = TextNode(block[4:-3], TextType.CODE)
+            html_node = text_node_to_html_node(text_node)
+            pre_node = ParentNode("pre", [html_node])
+            nodes.append(pre_node)
+        elif block_type == BlockType.HEADING:
+            html_node = get_html_heading(block)
+            nodes.append(html_node)
+        elif block_type == BlockType.QUOTE:
+            html_node = get_html_quote(block)
+            nodes.append(html_node)
+        elif block_type == BlockType.UNORDERED_LIST:
+            html_node = get_html_unordered_list(block)
+            nodes.append(html_node)
+        elif block_type == BlockType.ORDERED_LIST:
+            html_node = get_html_ordered_list(block)
+            nodes.append(html_node)
+        else:
+            block = block.replace("\n", " ")
+            html_node = ParentNode("p", text_to_children(block))
+            nodes.append(html_node)
+    root_node = ParentNode("div", nodes)
+    return root_node
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    child_nodes = [text_node_to_html_node(n) for n in text_nodes]
+    return child_nodes
+
+def get_html_heading(text):
+    heading_count = 0
+    while text[0] == "#":
+        heading_count += 1
+        text = text[1:]
+    text = text.strip()
+    nodes = text_to_children(text)
+    return ParentNode(f"h{heading_count}", nodes)
+
+def get_html_quote(text):
+    lines = text.split("\n")
+    text = ""
+    for line in lines:
+        text += f" {line[1:].strip()}"
+    nodes = text_to_children(text[1:])
+    return ParentNode("blockquote", nodes)
+
+def get_html_unordered_list(text):
+    lines = text.split("\n")
+    items = []
+    for line in lines:
+        items.append(ParentNode("li", text_to_children(line[2:])))
+    return ParentNode("ul", items)
+
+def get_html_ordered_list(text):
+    lines = text.split("\n")
+    items = []
+    for line in lines:
+        items.append(ParentNode("li", text_to_children(line[3:])))
+    return ParentNode("ol", items)
