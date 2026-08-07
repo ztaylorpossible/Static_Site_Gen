f9c134a3141881615b33ad0241ab6448c196ca53
ztaylorpossible <zachtaylorstudios@gmail.com>
2026-08-07 10:47:12 -0700

    feat: Generate pages recursively

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,5 @@
 import os
+from os.path import isdir
 
 from texttohtml import markdown_to_html_node
 
@@ -32,3 +33,16 @@ def generate_page(from_path, template_path, dest_path):
     html = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
     write_to_file(dest_path, html)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    contents = os.listdir(dir_path_content)
+    for entry in contents:
+        full_path = os.path.join(os.path.abspath(dir_path_content), entry)
+        if os.path.isfile(full_path):
+            file_path = os.path.join(dir_path_content, entry)
+            new_entry = entry.split(".")[0] + ".html"
+            dest_path = os.path.join(dest_dir_path, new_entry)
+            generate_page(file_path, template_path, dest_path)
+        elif os.path.isdir(full_path):
+            next_content = os.path.join(dir_path_content, entry)
+            next_dest = os.path.join(dest_dir_path, entry)
+            generate_pages_recursive(next_content, template_path, next_dest)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 from copydir import copy_dir_contents
 
 def main():
     copy_dir_contents("static/", "public/")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 main()
